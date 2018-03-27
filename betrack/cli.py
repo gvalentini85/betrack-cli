e7b23bd90eb77d5b1931ee42be46d40ddb819165
Gabriele Valentini <gabriele.valentini.85@gmail.com>
2018-03-27 16:46:39 -0700

    Setting up project structure

---
+++ betrack/cli.py
@@ -0,0 +1,42 @@
+"""
+betrack
+
+Usage:
+  betrack hello
+  betrack -h | --help
+  betrack --version
+
+Options:
+  -h --help                         Show this screen.
+  --version                         Show version.
+
+Examples:
+  betrack hello
+
+Help:
+  For help using this tool, please open an issue on the Github repository:
+  https://github.com/gvalentini85/betrack-cli/issues
+"""
+
+
+from inspect import getmembers, isclass
+
+from docopt import docopt
+
+from . import __version__ as VERSION
+
+
+def main():
+    """Main CLI entrypoint."""
+    import betrack.commands
+    options = docopt(__doc__, version=VERSION)
+
+    # Here we'll try to dynamically match the command the user is trying to run
+    # with a pre-defined command class we've already created.
+    for (k, v) in options.items(): 
+        if hasattr(betrack.commands, k) and v:
+            module = getattr(betrack.commands, k)
+            betrack.commands = getmembers(module, isclass)
+            command = [command[1] for command in betrack.commands if command[0] != 'Base'][0]
+            command = command(options)
+            command.run()
---
+++ setup.py
@@ -0,0 +1,71 @@
+"""Packaging settings."""
+
+
+from codecs import open
+from os.path import abspath, dirname, join
+from subprocess import call
+
+from setuptools import Command, find_packages, setup
+
+from betrack import __version__
+
+
+this_dir = abspath(dirname(__file__))
+with open(join(this_dir, 'README.rst'), encoding='utf-8') as file:
+    long_description = file.read()
+
+
+class RunTests(Command):
+    """Run all tests."""
+    description = 'run tests'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        """Run all tests!"""
+        errno = call(['py.test', '--cov=betrack', '--cov-report=term-missing'])
+        raise SystemExit(errno)
+
+
+setup(
+    name = 'betrack',
+    version = __version__,
+    description = 'A Python command line toolkit to track collective behaviors.',
+    long_description = long_description,
+    url = 'https://github.com/gvalentini85/betrack-cli',
+    author = 'Gabriele Valentini',
+    author_email = 'gvalent3@asu.edu',
+    license = 'MIT',
+    classifiers = [
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords = 'cli',
+    packages = find_packages(exclude=['docs', 'tests*']),
+    install_requires = ['docopt'],
+    extras_require = {
+        'test': ['coverage', 'pytest', 'pytest-cov'],
+    },
+    entry_points = {
+        'console_scripts': [
+            'betrack=betrack.cli:main',
+        ],
+    },
+    cmdclass = {'test': RunTests},
+)
