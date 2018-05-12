c9a8540c42eac56561ab1263bf95088fda6ec419
Gabriele Valentini <gabriele.valentini.85@gmail.com>
2018-05-12 16:25:03 -0700

    Structured run method of TrackParticles

--- betrack/commands/trackparticles.py
+++ betrack/commands/trackparticles.py
@@ -11,6 +11,31 @@ class TrackParticles(BetrackCommand):
     """Say hello, world!"""
 
     def run(self):
+
+        # Parse options and get list of jobs..
+        print('\033[01m' + '\033[94m' + 'Parsing parameters..' + '\x1b[0m')
+        
+        # Loop over jobs..
+        for job in range(0, 10):
+            job + 1
+            # Retrieve job-specific parameters..
+
+            # Open video..
+
+            # Preprocess video..
+
+            # Locate features..
+
+            # Link trajectories..
+
+            # Filter trajectories..
+
+            # Saving trajectories..
+
+            # Annotate video..
+
+            # Clean up..
+        
         print('TrackParticles: Hello, world!')
         print('You supplied the following options:',
               dumps(self.options, indent=2, sort_keys=True))
