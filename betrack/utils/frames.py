758a65cf5c976a18241f1143c5a400fc68bcdd5f
Gabriele Valentini <gabriele.valentini.85@gmail.com>
2018-05-12 12:08:00 -0700

    Added files for frames module

---
+++ betrack/utils/frames.py
@@ -0,0 +1,29 @@
+# Add copyright here..
+"""
+Description of the frames module..
+"""
+
+# Imports
+#from json import dumps
+#from .base import Base
+
+
+def crop(self):
+    """
+#    Compute the range of a continuously-valued time series.
+#    
+#    Examples: ::
+#    
+#        >>> from pyinform import utils
+#        >>> utils.series_range([0,1,2,3,4,5])
+#        (5, 0, 5)
+#        >>> utils.series_range([-0.1, 8.5, 0.02, -6.3])
+#        (14.8, -6.3, 8.5)
+#    :param sequence series: the time series
+#    :returns: the range and the minimum/maximum values
+#    :rtype: 3-tuple (float, float, float)
+#    :raises InformError: if an error occurs within the ``inform`` C call
+    """
+    
+    print('Hello, world!')
+    print('You supplied the following options:')
