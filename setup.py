e089e9069f6e0680d9d3d064ce7d798465ac7acd
Gabriele Valentini <gabriele.valentini.85@gmail.com>
2018-05-25 11:17:13 -0700

    Fix setup.py to require opencv

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
     ],
     keywords = 'cli',
     packages = find_packages(exclude=['docs', 'tests*']),
-    install_requires = ['docopt', 'pyyaml', 'pims', 'trackpy', 'tqdm', 'opencv'],
+    install_requires = ['docopt', 'pyyaml', 'pims', 'trackpy', 'tqdm', 'opencv-python'],
     extras_require = {
         'test': ['coverage', 'pytest', 'pytest-cov', 'codecov'],
     },
